2ac6f35b85102ed7626ea5afd79984775307bc78
finnhodgkin <finnhodgkin@gmail.com>
2017-10-06 17:00:41 +0100

    adds a first draft of the OAuth flow functions #6 #7

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,7 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :elixir_auth_github,
+  client_id: "ef6730c4c96c4eb6f818",
+  client_secret: "792c4ffd43db87a650d156400d8a207934ca64d5"
--- lib/elixir_auth_github.ex
+++ lib/elixir_auth_github.ex
@@ -3,16 +3,47 @@ defmodule ElixirAuthGithub do
   Documentation for ElixirAuthGithub.
   """
 
-  @doc """
-  Hello world.
+  @client_id Application.get_env(:elixir_auth_github, :client_id)
+  @client_secret Application.get_env(:elixir_auth_github, :client_secret)
 
-  ## Examples
-
-      iex> ElixirAuthGithub.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def login_url do
+    case @client_id do
+      nil ->
+        IO.puts "ENVIRONMENT VARIABLES NOT SET"
+      _ ->
+        "https://github.com/login/oauth/authorize?client_id=#{@client_id}"
+    end
   end
+
+  def login_url(state) do
+    case @client_id do
+      nil ->
+        IO.puts "ENVIRONMENT VARIABLES NOT SET"
+      _ ->
+        "https://github.com/login/oauth/authorize?client_id=#{@client_id}&state=#{state}"
+    end
+  end
+
+  def github_auth(code) do
+    url = "https://github.com/login/oauth/access_token?"
+    queryParams =
+      URI.encode_query(%{"client_id" => @client_id,
+                         "client_secret" => @client_secret,
+                         "code" => code})
+
+    %{body: accessQuery} =
+      HTTPoison.post!(url <> queryParams, "")
+
+    %{"access_token" => access_token} =
+      URI.decode_query(accessQuery)
+  end
+
+  def get_user_details(access_token) do
+    HTTPoison.get!("https://api.github.com/user", [
+      {"User-Agent", "elixir-practice"},
+      {"Authorization", "token #{access_token}"}
+    ])
+    |> IO.inspect
+  end
+
 end
