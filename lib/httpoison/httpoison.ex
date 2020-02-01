180bed96f5e4b69febc785523b52b7cf98bf2f28
nelsonic <nelson+github@dwyl.io>
2020-02-01 19:51:15 +0000

    update version of ex_doc in mix.lock #32

--- lib/httpoison/httpoison.ex
+++ lib/httpoison/httpoison.ex
@@ -1,13 +1,13 @@
-# defmodule ElixirAuthGithub.HTTPoison.InMemory do
-#   @moduledoc """
-#   In-memory storage for data while it's being processed.
-#   Because we are mocking the api requests in ElixirAuthGithub.HTTPoison.InMemory
-#   we have a separate module to delegate the functions we use to the actual
-#   HTTPoison module, so that's all we do here.
-#   Note: if you have a suggestion of a better way to do this, please share!
-#   """
-#
-#   defdelegate get!(url, headers, options \\ []), to: HTTPoison
-#
-#   defdelegate post!(url, body, headers \\ [], options \\ []), to: HTTPoison
-# end
+defmodule ElixirAuthGithub.HTTPoison.InMemory do
+  @moduledoc """
+  In-memory storage for data while it's being processed.
+  Because we are mocking the api requests in ElixirAuthGithub.HTTPoison.InMemory
+  we have a separate module to delegate the functions we use to the actual
+  HTTPoison module, so that's all we do here.
+  Note: if you have a suggestion of a better way to do this, please share!
+  """
+
+  defdelegate get!(url, headers, options \\ []), to: HTTPoison
+
+  defdelegate post!(url, body, headers \\ [], options \\ []), to: HTTPoison
+end
